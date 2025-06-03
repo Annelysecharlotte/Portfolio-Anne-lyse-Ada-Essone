dbab49fb0142ac221503d545fcabc072467da097
Annelysecharlotte <anne.lyse.charlotte.ada.essone@coursdiderot.com>
2025-06-03 23:20:34 +0200

    ajout d'image et modification

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Script JavaScript
+function openModal(id) {
+document.getElementById(id).style.display = 'block';
+}
+
+function closeModal(id) {
+document.getElementById(id).style.display = 'none';
+}
+
+// Fermer en cliquant hors de la modale
+window.onclick = function(event) {
+const modals = document.querySelectorAll('.modal');
+modals.forEach(modal => {
+    if (event.target === modal) {
+    modal.style.display = 'none';
+    }
+});
+};
